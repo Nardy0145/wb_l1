bdffde59ce95c4000567277bbe10be4ff04a6f44
Nardy0145 <nardy0145@gmail.com>
2022-08-20 19:23:38 +0300

    some theory

--- 22/main.go
+++ 22/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/big"
 )
 
 // Разработать программу, которая перемножает, делит, складывает, вычитает две числовых переменных a,b, значение которых > 2^20
 
 func main() {
-	a := math.Pow(2, 21)
-	b := math.Pow(2, 21)
-	fmt.Println(a * b)
-	fmt.Println(a + b)
-	fmt.Println(a - b)
-	fmt.Println(a / b)
+	a := new(big.Int)
+	a.SetString("48000000000000000000", 10)
+	b := new(big.Int)
+	b.SetString("24000000000000000000", 10)
+	r := new(big.Int)
+	fmt.Println(r.Add(a, b)) // +
+	fmt.Println(r.Sub(a, b)) // -
+	fmt.Println(r.Mul(a, b)) // *
+	fmt.Println(r.Div(a, b)) // /
 }
--- Theory testing/11/main.go
+++ Theory testing/11/main.go
@@ -9,12 +9,15 @@ func main() {
 	wg := sync.WaitGroup{}
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
+
 		go func(wg *sync.WaitGroup, i int) {
 			fmt.Println(i)
 			wg.Done()
+
 		}(&wg, i)
 	}
 	wg.Wait()
+
 	fmt.Println("exit")
 }
 
